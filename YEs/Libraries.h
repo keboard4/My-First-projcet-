cca03ef8dfde25a3bde6d6068d612f5ca0950993
nut master <nut master@DESKTOP-PH0PH2I>
2026-05-10 20:13:30 -0400

    Add project files.

---
+++ YEs/Libraries.h
@@ -0,0 +1,11 @@
+/*
+   Script name: Libraries.h
+   Desciption: Idk
+   Created on: 10 May 2026
+*/
+
+#include <iostream>
+
+#ifndef Libraries
+
+#endif // !Libraries
---
+++ YEs/index.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Libraries.h"
+
+using namespace std;
+
+int main() {
+	// Nothing
+
+	cout << "My Script is the best" << endl;
+
+	return 0;
+}
